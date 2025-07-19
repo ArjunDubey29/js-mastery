6d0e1c2765c085c6a19033aff36b51a8a60a899e
ArjunDubey29 <arjundubey672@gmail.com>
2025-07-19 09:35:31 +0000

    Conversions

---
+++ 01_Basics/03_conversions.js
@@ -0,0 +1,33 @@
+// let score = "33abc"
+// let score = null
+// let score = undefined
+let score = true
+
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN => type(Number hi hai)
+// true => 1; false => 0
+// null => 0
+// undifined => NaN
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false; "Arjun" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
+console.log(stringNumber);
