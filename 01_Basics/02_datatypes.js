58d9cda54db4081c4685de9da9711203d4857f7e
ArjunDubey29 <arjundubey672@gmail.com>
2025-07-19 08:45:27 +0000

    Datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //Treat all JS code as new JS
+// alert(3 + 3) // we are using node.js not browser
+console.log(3 + 3)
+console.log("Arjun") //Good code practice
+
+let name = "Arjun"
+let age = 19
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint => used for very big numbers
+// string => used for character array ""
+// boolean => true or false
+// null => in JS it is a stand alonevalue and a Object
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof "Arjun");
+console.log(typeof age);
+console.log(typeof null); // op: object
+console.log(typeof undefined); // op: undefined
