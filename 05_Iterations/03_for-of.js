9ddb8196e572083ff56f689e44a0531a5147cb8d
ArjunDubey29 <arjundubey672@gmail.com>
2025-07-24 16:04:48 +0000

    03_for-of

---
+++ 05_Iterations/03_for-of.js
@@ -0,0 +1,36 @@
+// For of loop
+// ["", "", ""]
+// [{}, {}, {}]
+
+const myArray = [1,2,3,4,5,6,7,8,9]
+for (const val of myArray) {
+    // console.log(val);
+}
+
+const myNewArray = "Arjun Dubey"
+for (const greet of myNewArray) {
+    // console.log(`Each character is: ${greet}`);
+}
+
+// Maps
+// const map = new Map()
+// // Stores Unique Values
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('FR', "France")
+
+// console.log(map);
+
+// for (const [key, value] of map) { //Array ka destructure ho jata hai yahan pe
+//     console.log(key, ':-', value);
+// }
+
+// On object
+// const myObject = {
+//     game1: 'NFS',
+//     game2: 'RDR2'
+// }
+
+// for (const [key, value] of myObject) {
+//     console.log(`${key} ':-' ${value}`);
+// } Not working
