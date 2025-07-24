047aee4b36581aef2e2e1d8ba34107b22d69e080
ArjunDubey29 <arjundubey672@gmail.com>
2025-07-24 13:12:06 +0000

    02_while-loop

---
+++ 05_Iterations/02_while_loop.js
@@ -0,0 +1,28 @@
+// While
+
+let index = 1
+// while (index <= 10) {
+//     console.log(`The value of i is: ${index}`);
+//     index++;
+// }
+
+let myHero = ["Batman", "Superman", "Flash", "Hulk", "Ironman"]
+
+let arr = 0 
+// while (arr < myHero.length) {
+//     console.log(myHero[arr]);
+//     arr++;
+// }
+
+do {
+    console.log(`The value is ${myHero[arr]}`);
+    arr++;
+} while (arr < myHero.length);
+
+
+// Special Case
+let score = 11
+do {
+    console.log(`The value is ${score}`);
+    score++;
+} while (score < 10);
